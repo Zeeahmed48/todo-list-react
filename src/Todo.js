89b06823b94a1b89ea0d0e3d372fe0f781c7e0fc
Zeeahmed48 <muhammadzeeshan26.mz@gmail.com>
2021-04-25 04:25:28 +0500

    added Todo.js file

---
+++ src/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Todo = ({todo}) => {
+    return (
+        <div>
+            {/* <ul>
+        {todo.map((todo) => {
+          return <li>{todo}</li>;
+        })}
+      </ul> */}
+      <ul className="todo__item">
+          <li>1</li>
+      </ul>
+        </div>
+    )
+}
+
+export default Todo
